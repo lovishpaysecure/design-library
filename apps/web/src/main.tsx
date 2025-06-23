66fe7b9df02341df91fbb21a3761d60f73e7ba28
LOVISH <lovish@LOVISHs-MacBook-Air-2.local>
2025-06-23 14:31:48 +0530

    fix build and versions

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App  from './App';
-import { initializeTokenSystem } from "@paysecure-design/tokens";
 
 // Import world map data for ECharts geographic charts
 // Note: Commented out due to Vite resolution issues with echarts-countries-js
 // import 'echarts-countries-js/world';
 
-// Initialize the token system
-initializeTokenSystem().then(() => {
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-});
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
  
